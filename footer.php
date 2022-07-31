4dc9e73af7af05b238e237a409c11694767ecb02
Jesús Gutiérrez Matarrita <jesus.gutierrezmatarrita@outlook.com>
2022-07-31 01:53:08 -0600

    Add project boilerplate

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
-<footer>
-    
+<footer class="bg-light text-center text-lg-start">
+  <!-- Credits -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Estudiante: Jesús Antonio Gutiérrez Matarrita. Si desea revisar el código en Github, vaya
+    <a class="text-dark" href="https://github.com/jesus-gutierrezmatarrita/tarea_bayes">aquí</a>
+  </div>
+  <!-- Copyright -->
 </footer>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php include('header.php') ?>
 <body>
-    
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Bayes</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="formulario1.php">Formulario 1</a>
+            </li>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
 </body>
+<?php include('footer.php') ?>
